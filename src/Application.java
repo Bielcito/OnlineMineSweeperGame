9fbc54017a1584ca015ac7b3be8b0c739ee00fd7
Bielcito <oliveira.grdo@gmail.com>
2016-10-15 16:25:57 -0300

    README colocado no lugar certo

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,4 @@
-import java.awt.Desktop;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -10,26 +8,53 @@ public class Application {
 	{		
 		try
 		{
-			String command = "java -jar C:\\Users\\Bielcito\\Documents\\Projetos\\\\\"Eclipse Projects\"\\OnlineMineSweeperGame\\src\\portmapper-2.0.0 -add -externalPort 12345 -internalPort 12345 -protocol tcp";
-			
-			/*game = new Game(5, 5, 3);
-			game.start();*/
-			ProcessBuilder builder = new ProcessBuilder(
-		            "cmd.exe", "/c", command);
-		        builder.redirectErrorStream(true);
-		        Process p = builder.start();
-		        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		        String line;
-		        while (true) {
-		            line = r.readLine();
-		            if (line == null) { break; }
-		            System.out.println(line);
-		        }
+			game = new Game(5, 5, 3);
+			game.start();
 			
 		}
 		catch (Exception e) 
 		{
-			e.getMessage();
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void createNewPortMapping(String externalPort, String internalPort)
+	{
+		try {
+			String command = "java -jar C:\\Users\\Bielcito\\Documents\\Projetos\\\\\"Eclipse Projects\"\\OnlineMineSweeperGame\\src\\portmapper-2.0.0 -add -externalPort "+ externalPort + " -internalPort " + internalPort + " -protocol tcp";
+			ProcessBuilder builder = new ProcessBuilder(
+			        "cmd.exe", "/c", command);
+			    builder.redirectErrorStream(true);
+			    Process p = builder.start();
+			    BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			    String line;
+			    while (true) {
+			        line = r.readLine();
+			        if (line == null) { break; }
+			        System.out.println(line);
+			    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void createNewPortMapping(String externalPort, String internalPort, String ip)
+	{
+		try {
+			String command = "java -jar C:\\Users\\Bielcito\\Documents\\Projetos\\\\\"Eclipse Projects\"\\OnlineMineSweeperGame\\src\\portmapper-2.0.0 -add -externalPort "+ externalPort + " -internalPort " + internalPort + " -ip "+ ip + " -protocol tcp";
+			ProcessBuilder builder = new ProcessBuilder(
+			        "cmd.exe", "/c", command);
+			    builder.redirectErrorStream(true);
+			    Process p = builder.start();
+			    BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			    String line;
+			    while (true) {
+			        line = r.readLine();
+			        if (line == null) { break; }
+			        System.out.println(line);
+			    }
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
